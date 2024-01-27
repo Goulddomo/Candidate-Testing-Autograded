d26a04e3845d1f8a2955a460ed062cf5f1e63d51
Dominique Gould <dominiquegould42@yahoo.com>
2024-01-27 11:37:29 -0600

    Test should work

--- candidate-testing.js
+++ candidate-testing.js
@@ -57,7 +57,7 @@ function askQuestion() {
   candidateAnswers[0] = input.question(questions[0]);
 
 // while (candidateAnswers[0] !== correctAnswers[0]){
-  if (candidateAnswers[0].toUpperCase === correctAnswers[0].toUpperCase){
+  if (candidateAnswers[0].toUpperCase() === correctAnswers[0].toUpperCase()){
     numCorrect.push(candidateAnswers[0]);
     console.log("Correct.");
     // break;
@@ -71,7 +71,7 @@ function askQuestion() {
 candidateAnswers[1] = input.question(questions[1]);
 
 // while (candidateAnswers[1] !== correctAnswers[1]){
-  if (candidateAnswers[1].toUpperCase === correctAnswers[1].toUpperCase){
+  if (candidateAnswers[1].toUpperCase() === correctAnswers[1].toUpperCase()){
     console.log("Correct.");
     numCorrect.push(candidateAnswers[1]);
     // break;
@@ -98,7 +98,7 @@ candidateAnswers[2] = input.question(questions[2]);
 candidateAnswers[3] = input.question(questions[3]);
 
 // while (candidateAnswers[3] !== correctAnswers[3]){
-  if (candidateAnswers[3].toUpperCase === correctAnswers[3].toUpperCase){
+  if (candidateAnswers[3].toUpperCase() === correctAnswers[3].toUpperCase()){
     numCorrect.push(candidateAnswers[3]);
     console.log("Correct.");
     // break;
@@ -120,7 +120,7 @@ candidateAnswers[4] = input.question(questions[4]);
   // candidateAnswers[4] = input.question(questions[4]);
   // break;
 }
-console.log(numCorrect);
+// console.log(numCorrect);
 }
 
 function gradeQuiz(candidateAnswers) {
